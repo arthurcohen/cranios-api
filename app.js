b505019b4e192acae544954e724c34764fa0209a
Arthur Cohen Costa Dos Santos <cohen@A305-20L.imd.local>
2019-09-09 21:44:11 -0300

    Added swagger ui for documenting

--- app.js
+++ app.js
@@ -5,23 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-const dbConfig = require('./config/config.json')
-
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(dbConfig.development)
-const { User } = require('./models')
-
-// sequelize.authenticate()
-//     .then(async () => {
-//         console.log('ihu')
-//         // let a = await User.create({username: 'cohen', email: 'arthur@gmail', name: 'Arthur', participation: 5.0, password: '123'});
-//         // console.log(a);
-//         // a.name = 'Arthur 2';
-//         // console.log(a);
-//         // console.log(await User.findAll());
-//     })
-//     .catch((err) => console.error('oops', err))
+var swaggerRouter = require('./routes/swagger');
 
 var app = express();
 
@@ -33,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/swagger-ui', swaggerRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,88 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models');
 
+/**
+ * @swagger
+ *
+ * definitions:
+ *   NewUser:
+ *     type: object
+ *     required:
+ *       - email
+ *       - username
+ *       - password
+ *       - name
+ *     properties:
+ *       email:
+ *         type: string
+ *       username:
+ *         type: string
+ *       password:
+ *         type: string
+ *         format: password
+ *       name:
+ *         type: string
+ *       picture:
+ *         type: string
+ *       city:
+ *         type: string
+ *       participation:
+ *         type: number
+ * 
+ *   User:
+ *     allOf:
+ *       - $ref: '#/definitions/NewUser'
+ *       - properties:
+ *         id:
+ *           type: number
+ *         createdAt:
+ *           type: date
+ *         updatedAt:
+ *           type: date
+ */
 
-/* GET users listing. */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     description: This should return all users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: All users
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/User'
+ *       404:
+ *         description: No users found
+ */
 router.get('/', async function(req, res, next) {
   res.send(await db.User.findAll());
 });
 
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     description: This should create an user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: User
+ *         required: true
+ *         in: body
+ *         schema:
+ *           $ref: '#/definitions/NewUser'
+ *     responses:
+ *       200:
+ *         description: User created
+ *       422:
+ *         description: Failed to save user
+ */
 router.post('/', async function(req, res, next) {
   const user = await db.User.create(req.body);
   res.send(user);
